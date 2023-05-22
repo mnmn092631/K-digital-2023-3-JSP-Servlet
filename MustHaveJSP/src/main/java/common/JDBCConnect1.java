3ee9be99ecb07bae1dfd2d61d6405d0c4e21cbff
juhee kang <mnmn092631@gmail.com>
2023-05-22 14:54:46 +0900

    JDBCConnect 수정

--- MustHaveJSP/src/main/java/common/JDBCConnect1.java
+++ MustHaveJSP/src/main/java/common/JDBCConnect1.java
@@ -2,40 +2,54 @@ package common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
+import javax.servlet.ServletContext;
 
 public class JDBCConnect1 {
-	
-	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url = "jdbc:mysql://localhost:3306/musthave";
-	private String id = "musthave";
-	private String pw = "tiger";
-	
-	public Connection getConnection() throws Exception {
-		Class.forName(driver);
-		return DriverManager.getConnection(url, id, pw);
+
+	private String driver;
+	private String url;
+	private String id;
+	private String pw;
+
+	public JDBCConnect1() {
+		driver = "com.mysql.cj.jdbc.Driver";
+		url = "jdbc:mysql://localhost:3306/musthave";
+		id = "musthave";
+		pw = "tiger";
 	}
-	
-	public static void main(String[] args) throws Exception {
-		JDBCConnect1 jdbcConnect = new JDBCConnect1();
-		Connection con = jdbcConnect.getConnection();
-		
-		Statement st = con.createStatement();
-		
-		ResultSet rs = st.executeQuery("select * from board");
-		while(rs.next()) {
-			System.out.println(String.format("%s. %s, %s", rs.getString("title"), rs.getString("content"), rs.getString("id")));
-		}
-		
-		System.out.println();
-		
-		ResultSet rs1 = st.executeQuery("select * from member");
-		while(rs1.next()) {
-			System.out.println(String.format("%s. %s, %s", rs1.getString("id"), rs1.getString("pass"), rs1.getString("name")));
-		}
-		
-		con.close();
+
+	public JDBCConnect1(String driver, String url, String id, String pw) {
+		this.driver = driver;
+		this.url = url;
+		this.id = id;
+		this.pw = pw;
 	}
-	
+
+	public JDBCConnect1(ServletContext app) {
+		driver = app.getInitParameter("MySQLDriver");
+		url = app.getInitParameter("MySQLURL");
+		id = app.getInitParameter("MySQLId");
+		pw = app.getInitParameter("MySQLPw");
+	}
+
+	public Connection getConnection() {
+		try {
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url, id, pw);
+			return con;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void closeConnection(Connection con) {
+		try {
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
